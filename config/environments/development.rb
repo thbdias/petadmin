325852c5d0397305b904647fd9c2f1fbfdb6ea6d
thbdias <thiagobalbino91@gmail.com>
2019-02-20 06:28:07 -0300

    setando o mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # configurando o action_mails do rails 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' } # local onde o site está no momento
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => 'mailcatcher', :port => 1025 } # mailcatcher: nome dentro do container
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
