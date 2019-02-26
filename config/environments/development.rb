66b5f76b6017a81f8cb8b91e5db947c329bbde01
thbdias <thiagobalbino91@gmail.com>
2019-02-26 12:51:21 -0300

    customizando o comportamento 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,4 +56,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  
+  
+  # config.logger = ActiveSupport::Logger.new(STDOUT)
+  
 end
